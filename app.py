2160ba56930016d8fccb1f4a55d07e9d2e4a7fb2
Abinaya-120195 <153588319+Abinaya-120195@users.noreply.github.com>
2024-12-11 01:12:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-# app.py
-def greet():
-    return "Hello, AWS Abi!"
+# app/main.py
+from flask import Flask
 
-if __name__ == "__main__":
-    print(greet())
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
